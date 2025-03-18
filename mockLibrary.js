e3ed7ddbca8322bc44489c27ce518fb972a0673f
TKAkhter <talhaakhter01@gmail.com>
2025-03-18 16:18:27 +0500

    minor changes added

--- mockLibrary.js
+++ mockLibrary.js
@@ -1,8 +1,20 @@
 const MockLibrary = {
-    connect: () => console.log("Connected (v2)"),
-    disconnect: () => console.log("Disconnected (v2)"),
-    isConnected: () => true,
-    batteryStatus: () => 85,
+    connect: () => {
+        console.log("Connected (v2)");
+        return "Connected (v2)"
+    },
+    disconnect: () => {
+        console.log("Disconnected (v2)");
+        return "Disconnected (v2)"
+    },
+    isConnected: () => {
+        console.log("Connected: true (v2)");
+        return true
+    },
+    batteryStatus: () => {
+        console.log("Battery Status: 85% (v2)");
+        return 85
+    },
 };
 
 module.exports = MockLibrary;
