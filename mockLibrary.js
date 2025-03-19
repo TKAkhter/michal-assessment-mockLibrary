6eb4d2906e7adab9befb6017f32565ea42ec1a0c
TKAkhter <talhaakhter01@gmail.com>
2025-03-19 15:26:39 +0500

    minor changes added

--- mockLibrary.js
+++ mockLibrary.js
@@ -1,11 +1,9 @@
 const MockLibrary = {
     connection: () => {
         console.log("Connected (v1)")
-        return "Connected (v1)"
     },
     disconnection: () => {
         console.log("Disconnected (v1)")
-        return "Disconnected (v1)"
     },
     connected: () => {
         console.log("Connected: true (v1)")
